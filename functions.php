5e8b424c0befab8915b1df4847c8abc14391aa8f
isabelle may salmon <845868806@bun2-206-13.local>
2018-11-30 11:31:49 +1300

    set up base files

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+
+// Custom scripts & styles
+function addCustomThemeStyles(){
+  // Style
+  wp_enqueue_style('googlefonts', 'https://fonts.googleapis.com/css?family=Montserrat|Oswald:700', array(), '0.0.1', 'all');
+  wp_enqueue_style('fontawesome', 'https://use.fontawesome.com/releases/v5.4.2/css/all.css', array(), '5.4.2', 'all');
+  wp_enqueue_style('maincss', get_template_directory_uri().'/assets/main.css', array(), '0.0.1', 'all');
+  // Scripts
+  wp_enqueue_script('customscripts', get_template_directory_uri().'/assets/custom-script.js', array(), '0.0.1', true);
+
+  global $wp_query;
+}
+
+add_action('wp_enqueue_scripts', 'addCustomThemeStyles');
+
+// Custom menus
+function addCustomMenus(){
+  add_theme_support('menus');
+  register_nav_menu('header_nav', 'Hamburger menu');
+}
+
+add_action('init', 'addCustomMenus');
---
+++ header-home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php wp_head(); ?>
+    <title><?= get_bloginfo('name'); ?></title>
+  </head>
+  <body>
+
+      <h1 class="site-title"><?= get_bloginfo('name'); ?></h1>
+      <div class="hamburger">
+        <i class="fas fa-bars"></i>
+      </div>
+    <div class="container">
