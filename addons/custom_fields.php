0a99f37a4115f3340c1aa474cc1e5e4cdba50616
isabelle may salmon <845868806@bun2-206-13.local>
2018-12-05 11:54:14 +1300

    added image and text custom fields

--- addons/custom_fields.php
+++ addons/custom_fields.php
@@ -1,18 +1,122 @@
 <?php
 
-// Custom meta boxes
-
 $metaboxes = array(
-    'web_ux_section' => array(
-        'title' => __('Project Content', 'Breadcrumbs'),
-        'applicableto' => 'webstudents',
+    'service_images' => array(
+        'title' => __('Upload image', 'Breadcrumbs'),
+        'applicableto' => 'servicesimages',
         'location' => 'normal',
         'priority' => 'high',
         'fields' => array(
-            'profile_link' => array(
-              'title' => __('Link to Linkedin: ', 'shPhotography'),
-              'type' => 'text'
+            'header_image' => array(
+                'title' => __('Project Image: ', 'shPhotography'),
+                'type' => 'image',
+                'description' => 'Upload an image to a service page.'
             )
         )
+    ),
+    'service-type' => array(
+      'title' => __('Service list', 'Breadcrumbs'),
+      'applicableto' => 'servicesimages',
+      'location' => 'normal',
+      'priority' => 'high',
+      'fields' => array(
+          'header_image' => array(
+              'title' => __('Select service: ', 'shPhotography'),
+              'type' => 'text',
+              'description' => 'Upload an image to a service page.'
+          )
+      )
     )
 );
+
+function add_post_format_metabox() {
+    global $metaboxes;
+    if ( ! empty( $metaboxes ) ) {
+        foreach ( $metaboxes as $id => $metabox ) {
+            add_meta_box( $id, $metabox['title'], 'show_metaboxes', $metabox['applicableto'], $metabox['location'], $metabox['priority'], $id );
+        }
+    }
+}
+
+add_action( 'admin_init', 'add_post_format_metabox' );
+
+function show_metaboxes( $post, $args ) {
+    global $metaboxes;
+
+    $custom = get_post_custom( $post->ID );
+    $fields = $tabs = $metaboxes[$args['id']]['fields'];
+
+    $output = '<input type="hidden" name="post_format_meta_box_nonce" value="' . wp_create_nonce( basename( __FILE__ ) ) . '" />';
+
+    if ( sizeof( $fields ) ) {
+        foreach ( $fields as $id => $field ) {
+            switch ( $field['type'] ) {
+                case 'text':
+                    $output .= '<div class="form-group"><label for="' . $id . '">' . $field['title'] . '</label></div>';
+                    $output .= '<p>Outputting text<p>';
+                break;
+                case 'image':
+                    $image =  get_post_meta( $post->ID, $id, true );
+                    if($image){
+                        $imagesrc = wp_get_attachment_image_url($image, 'service_image', false);
+                        $removeClasses = "remove_custom_images button";
+                    } else{
+                        $removeClasses = "remove_custom_images button hidden";
+                    }
+                    $output .= '<div class="image-form-group">';
+                        $output .= '<label>'.$field['title'].'</label>';
+                        $output .= '<img class="custom_image" src="'.$imagesrc.'">';
+                        $output .= '<input type="hidden" value="' . $custom[$id][0] . '" class="customInput regular-text process_custom_images" name="'.$id.'" max="" min="1" step="1" readonly style="display:block">';
+                        $output .= '<button class="set_custom_images button">Add Image</button>';
+                        $output .= '<button class="'.$removeClasses.'">Remove Image</button>';
+                    $output .= '</div>';
+                break;
+                default:
+                    $output .= '<div class="form-group"><label for="' . $id . '">' . $field['title'] . '</label><input class="customInput" id="' . $id . '" type="text" name="' . $id . '" value="' . $custom[$id][0] . '" style="width: 100%;" /></div>';
+                break;
+            }
+        }
+    }
+    echo $output;
+}
+
+
+function save_metaboxes( $post_id ) {
+    global $metaboxes;
+
+    if ( ! wp_verify_nonce( $_POST['post_format_meta_box_nonce'], basename( __FILE__ ) ) )
+        return $post_id;
+
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+        return $post_id;
+
+    if ( 'page' == $_POST['post_type'] ) {
+        if ( ! current_user_can( 'edit_page', $post_id ) )
+            return $post_id;
+    } elseif ( ! current_user_can( 'edit_post', $post_id ) ) {
+        return $post_id;
+    }
+    $post_type = get_post_type();
+
+    foreach ( $metaboxes as $id => $metabox ) {
+        // die($post_type);
+        if ( $metabox['applicableto'] == $post_type ) {
+            $fields = $metaboxes[$id]['fields'];
+
+            foreach ( $fields as $id => $field ) {
+                $old = get_post_meta( $post_id, $id, true );
+                $new = $_POST[$id];
+
+
+                if ( $new && $new != $old ) {
+                    update_post_meta( $post_id, $id, $new );
+                }
+                elseif ( '' == $new && $old || ! isset( $_POST[$id] ) ) {
+                    delete_post_meta( $post_id, $id, $old );
+                }
+            }
+        }
+    }
+
+}
+add_action( 'save_post', 'save_metaboxes' );
--- functions.php
+++ functions.php
@@ -6,8 +6,11 @@ function addCustomThemeStyles(){
   wp_enqueue_style('googlefonts', 'https://fonts.googleapis.com/css?family=Montserrat:300,400,600|Oswald:700', array(), '0.0.1', 'all');
   wp_enqueue_style('fontawesome', 'https://use.fontawesome.com/releases/v5.4.2/css/all.css', array(), '5.4.2', 'all');
   wp_enqueue_style('maincss', get_template_directory_uri().'/assets/main.css', array(), '0.0.1', 'all');
+  wp_enqueue_style('customFieldStyles', get_template_directory_uri().'/assets/customFieldStyles.css', array(), '0.0.1', 'all');
   // Scripts
+  wp_enqueue_script('jquery');
   wp_enqueue_script('customscripts', get_template_directory_uri().'/assets/custom-script.js', array(), '0.0.1', true);
+  wp_enqueue_script('customFields', get_template_directory_uri().'/assets/customFields.js', array(), '0.0.1', true);
 
   global $wp_query;
 }
@@ -29,8 +32,8 @@ add_action('init', 'addCustomMenus');
 function add_services_post_type(){
 
   $labels = array(
-    'name' => _x('Services', 'post type name', 'shPhotography'),
-    'singular_name' => _x('Services', 'post type singular name', 'shPhotography'),
+    'name' => _x('Add services', 'post type name', 'shPhotography'),
+    'singular_name' => _x('Add services', 'post type singular name', 'shPhotography'),
     'add_new_item' => _x('Add service', 'adding service type', 'shPhotography')
   );
 
@@ -54,4 +57,33 @@ function add_services_post_type(){
   register_post_type('services', $args);
 }
 
+function add_service_images_post_type(){
+
+  $labels = array(
+    'name' => _x('Upload services images', 'post type name', 'shPhotography'),
+    'singular_name' => _x('Upload services images', 'post type singular name', 'shPhotography'),
+    'add_new_item' => _x('Add image to service', 'adding service type', 'shPhotography')
+  );
+
+  $args = array(
+    'labels' => $labels,
+    'description' => 'a post type that creates uploads photos to the appropriate service page',
+    'public' => true,
+    'hierarchical' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'show_in_nav_menus' => false,
+    'menu_position' => 5,
+    'menu_icon' => 'dashicons-format-gallery',
+    'supports' => array(
+      'title',
+    ),
+    'query_var' => true
+  );
+  register_post_type('servicesimages', $args);
+}
+
+add_action('init', 'add_service_images_post_type');
 add_action('init', 'add_services_post_type');
+
+require get_parent_theme_file_path('/addons/custom_fields.php');
