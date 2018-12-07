e691824f549d4cdb49303409e2b52c600c9d33cd
isabelle may salmon <izsi.salmon@gmail.com>
2018-12-07 15:35:52 +1300

    created test page and got swiper working

--- single-services.php
+++ single-services.php
@@ -25,29 +25,31 @@
 
            <?php if( $serviceImgPosts->have_posts() ): ?>
 
-            <?php while($serviceImgPosts->have_posts()): $serviceImgPosts->the_post(); ?>
+             <div class="service-gallery">
+               <?php while($serviceImgPosts->have_posts()): $serviceImgPosts->the_post(); ?>
 
-              <?php $id = get_the_id(); ?>
-              <?php $alt = get_the_title(); ?>
-              <?php $serviceType =  get_post_meta( $id, 'service_type', true ); ?>
+                 <?php $id = get_the_id(); ?>
+                 <?php $alt = get_the_title(); ?>
+                 <?php $serviceType =  get_post_meta( $id, 'service_type', true ); ?>
 
-              <?php if($serviceType === $servicePage): ?>
-                <?php
-                $imageID =  get_post_meta( $id, 'service_image', true );
+                 <?php if($serviceType === $servicePage): ?>
+                   <?php
+                   $imageID =  get_post_meta( $id, 'service_image', true );
 
-                if($imageID){
-                   $image = wp_get_attachment_image_url($imageID, 'large', false);
-                 }
+                   if($imageID){
+                      $image = wp_get_attachment_image_url($imageID, 'large', false);
+                    }
 
-                 ?>
+                    ?>
 
-                <?php // if($image = ): ?>
-                  <img src="<?= $image ?>" alt="<?= $alt ?>" class="service-image">
-                <?php // endif; ?>
-              <?php endif; ?>
+                     <img src="<?= $image ?>" alt="<?= $alt ?>" class="service-image">
+
+                 <?php endif; ?>
 
 
-            <?php endwhile; ?>
+               <?php endwhile; ?>
+             </div>
+
            <?php endif; ?>
 
 <?php endif; ?>
